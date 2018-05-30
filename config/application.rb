99635778d84a25b08318288afdd9b7d948475e17
johnwojo <jwojo13@gmail.com>
2018-05-30 15:18:25 -0400

    Added .ssl to configure secure booting..

--- config/application.rb
+++ config/application.rb
@@ -7,15 +7,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Sensei
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+  module Sensei
+    class Application < Rails::Application
+      config.load_defaults 5.2
+      config.force_ssl = true
+    end
   end
 end
